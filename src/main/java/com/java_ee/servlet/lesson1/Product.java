b5e2a84042f5ba539d6b3699b272732dbb64c515
QAIliamaster <gogogo3435@gmail.com>
2024-04-10 21:03:02 +0800

    Сервлет

--- src/main/java/com/java_ee/servlet/lesson1/Product.java
+++ src/main/java/com/java_ee/servlet/lesson1/Product.java
@@ -1,2 +1,34 @@
-package com.java_ee.servlet.lesson1;public class Product {
+package com.java_ee.servlet.lesson1;
+
+import java.util.Random;
+
+public class Product {
+
+    private final String[] PRODUCT_NAME = {"LuxeBlend", "EcoHarbor", "VitaVista", "GloGrove", "CrispCraft",
+            "QuantuQuill", "ZestifyZen", "AquaSculpt", "FloraFlicker", "SwiftSpark", "SereniSip", "SolarLoom",
+            "PulsePetal", "ZephyrZoom", "BrioBurst", "NexaNourish", "EpicureEase", "VelvetVista", "AetherAura",
+            "RetroRipple"};
+
+    private int id;
+    private String title;
+    private int cost;
+
+    private static int idCounter = 0;
+    Random random = new Random();
+
+    public Product() {
+        this.id = idCounter++;
+        this.title = PRODUCT_NAME[random.nextInt(19)];
+        this.cost = random.nextInt();
+    }
+
+    public Product(int id, String title, int cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
--- src/main/java/com/java_ee/servlet/lesson1/ProductDisplayerServlet.java
+++ src/main/java/com/java_ee/servlet/lesson1/ProductDisplayerServlet.java
@@ -1,2 +1,25 @@
-package com.java_ee.servlet.lesson1;public class ProductDisplayerServlet {
+package com.java_ee.servlet.lesson1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ProductDisplayerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Product[] products = new Product[5];
+        for (int i = 0; i < products.length ; i++) {
+            products[i] = new Product();
+        }
+
+        resp.getWriter().printf("<html><body>");
+
+        for (int i = 0; i < products.length; i++) {
+            resp.getWriter().printf("<h1>" + products[i].getTitle() + "</h1>");
+        }
+        resp.getWriter().printf("</body></html>");
+    }
 }
